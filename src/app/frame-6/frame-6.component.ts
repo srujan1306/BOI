50aab23e3316d6e1194efa9ab4258de377cf0156
srujan1306 <srujan.pothu@proclink.com>
2024-09-24 11:10:04 +0530

    service offerings changed to dropdown for mobile screen

--- src/app/frame-6/frame-6.component.ts
+++ src/app/frame-6/frame-6.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ServiceOfferingsComponent } from '../service-offerings/service-offerings.component';
 
 @Component({
@@ -8,4 +8,23 @@ import { ServiceOfferingsComponent } from '../service-offerings/service-offering
   templateUrl: './frame-6.component.html',
   styleUrl: './frame-6.component.scss',
 })
-export class Frame6Component {}
+export class Frame6Component implements OnInit {
+  isMobile: boolean = false;
+
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isMobile = window.innerWidth < 480;
+    if (this.isMobile) {
+      // Your code for mobile screens here
+      console.log('Screen size is less than 480px');
+    }
+  }
+}
