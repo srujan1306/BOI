b60ed551bd64dbb5de81c776e1e6442229bbdf57
srujan1306 <srujan.pothu@proclink.com>
2024-09-27 15:50:58 +0530

    images on hover in frame 2 is done

--- src/app/frame-2/frame-2.component.ts
+++ src/app/frame-2/frame-2.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-frame-2',
   standalone: true,
   imports: [],
   templateUrl: './frame-2.component.html',
-  styleUrl: './frame-2.component.scss'
+  styleUrl: './frame-2.component.scss',
 })
 export class Frame2Component {
+  images = ['images/0.jpg', 'images/1.png', 'images/2.png', 'images/3.png'];
+  currentImage: string = 'images/0.jpg';
 
+  onMouseEnter(index: number) {
+    this.currentImage = this.images[index];
+  }
 }
