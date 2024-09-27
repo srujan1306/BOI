769035db264783cc32e487bf21c77423b8b965a7
srujan1306 <srujan.pothu@proclink.com>
2024-09-27 14:33:27 +0530

    dropdown for mobile view is also done properly

--- src/app/frame-6/frame-6.component.ts
+++ src/app/frame-6/frame-6.component.ts
@@ -1,19 +1,52 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { ServiceOfferingsComponent } from '../service-offerings/service-offerings.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CustomerdetailsService } from '../customerdetails.service';
+import { Router } from '@angular/router';
+import { newCustomer } from '../app.component';
 
 @Component({
   selector: 'app-frame-6',
   standalone: true,
-  imports: [ServiceOfferingsComponent],
+  imports: [ServiceOfferingsComponent, ReactiveFormsModule],
   templateUrl: './frame-6.component.html',
   styleUrl: './frame-6.component.scss',
 })
 export class Frame6Component implements OnInit {
   isMobile: boolean = false;
   isLoading: boolean = false;
+  usaStates: string[] = [];
+  newCustomerForm!: FormGroup;
+  isModalVisible = false;
+  package: string = '';
+  showSuccess: boolean = false;
+  errorMessage: string | null = null;
 
-  ngOnInit(): void {
+  constructor(
+    public CustomerdetailsService: CustomerdetailsService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    // formGroup -> formControlName
+  }
+  async ngOnInit() {
     this.checkScreenSize();
+
+    this.usaStates = await this.CustomerdetailsService.loadCountries();
+
+    this.newCustomerForm = this.fb.group({
+      fullname: ['', [Validators.required, Validators.minLength(2)]],
+      phone_number: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      state: ['', [Validators.required]],
+      email: ['', [Validators.email]],
+      company: ['', [Validators.required]],
+      package: this.package,
+    });
   }
 
   @HostListener('window:resize', ['$event'])
@@ -28,6 +61,7 @@ export class Frame6Component implements OnInit {
       console.log('Screen size is less than 480px');
     }
   }
+
   isDropdown_gold: boolean = false;
   isDropdown_diamond: boolean = false;
   isDropdown_platinum: boolean = false;
@@ -42,12 +76,68 @@ export class Frame6Component implements OnInit {
     }
   }
 
+  async newCustomer() {
+    if (this.newCustomerForm.invalid) {
+      this.errorMessage = 'Please fill in all the fields.';
+      return;
+    }
+    this.isLoading = true;
+    if (this.newCustomerForm.valid) {
+      this.clear_fieldmissing_error();
+
+      let customer_details: newCustomer = {
+        ...this.newCustomerForm.value,
+        phone_number: Number(this.newCustomerForm.value.phone_number), // Convert to number
+      };
+      try {
+        const userId = localStorage.getItem('id');
+        if (userId) {
+          const updateUser =
+            await this.CustomerdetailsService.updateCustomer_details(
+              customer_details
+            );
+          this.showSuccess = true;
+          this.newCustomerForm.reset();
+          localStorage.removeItem('id');
+          localStorage.removeItem('formData');
+        } else {
+          const new_user =
+            await this.CustomerdetailsService.newCustomer_details(
+              customer_details
+            );
+          this.showSuccess = true;
+          this.newCustomerForm.reset();
+          localStorage.removeItem('id');
+          localStorage.removeItem('formData');
+        }
+      } catch (error) {
+        console.error('Error sending customer details:', error);
+        // Optionally show an error message to the user
+      }
+    }
+    this.isLoading = false;
+  }
+
+  showModal() {
+    this.isModalVisible = true;
+    document.body.classList.add('modal-open');
+  }
+
+  closeModal() {
+    this.isModalVisible = false;
+    this.showSuccess = false;
+
+    document.body.classList.remove('modal-open');
+  }
   prefill_package(pack: string) {
-    // this.package = pack;
-    // this.newCustomerForm.patchValue({ package: this.package });
-    // this.newCustomerForm.patchValue(
-    //   JSON.parse(localStorage.getItem('formData') || '{}')
-    // );
-    // this.showModal();
+    this.package = pack;
+    this.newCustomerForm.patchValue({ package: this.package });
+    this.newCustomerForm.patchValue(
+      JSON.parse(localStorage.getItem('formData') || '{}')
+    );
+    this.showModal();
+  }
+  clear_fieldmissing_error() {
+    this.errorMessage = null;
   }
 }
